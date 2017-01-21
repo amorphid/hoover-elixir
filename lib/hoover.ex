f48a26f763918e8f8d89f3931efb1e7c916101f0
Michael Pope <mpope.cr@gmail.com>
2017-01-20 21:07:26 -0800

    Start hooving list of all DigitalOcean images

--- lib/hoover.ex
+++ lib/hoover.ex
@@ -1,18 +1,12 @@
 defmodule Hoover do
-  @moduledoc """
-  Documentation for Hoover.
-  """
+  @digital_ocean_api_key Application.fetch_env!(:hoover, :digital_ocean_api_key)
+  @hackney_options Application.fetch_env!(:hoover, :hackney_options)
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Hoover.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def hoove_digital_ocean_images_list do
+    HTTPoison.get!(
+      "https://api.digitalocean.com/v2/images",
+      [ "Authorization": "Bearer #{@digital_ocean_api_key}",
+        "Content-Type": "application/json"],
+      @hackney_options)
   end
 end
