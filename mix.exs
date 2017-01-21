ac433e6839b0415f4bd7571a522f4f3a388a3604
Michael Pope <mpope.cr@gmail.com>
2017-01-20 20:45:11 -0800

    Add httpoison

--- mix.exs
+++ mix.exs
@@ -1,34 +1,42 @@
 defmodule Hoover.Mixfile do
   use Mix.Project
 
-  def project do
-    [app: :hoover,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+  ##########
+  # Public #
+  ##########
+
+  def application() do
+    [
+      extra_applications: extra_applications(),
+      mod:                {Hoover.Application, []}
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
-     mod: {Hoover.Application, []}]
+  def project() do
+    [
+      app:              :hoover,
+      deps:             deps(),
+      elixir:           "~> 1.4",
+      build_embedded:   Mix.env == :prod,
+      start_permanent:  Mix.env == :prod,
+      version:          "0.1.0"
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+  ###########
+  # Private #
+  ###########
+
+  defp deps() do
+    [
+      {:httpoison, "~> 0.11.0"}
+    ]
+  end
+
+  defp extra_applications() do
+    [
+      :httpoison,
+      :logger
+    ]
   end
 end
